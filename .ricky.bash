ec06cb8aee51574fdc4333420cdd0c692d40658f
Ricky Pai <rpai@twitter.com>
2013-12-26 20:46:38 -0800

    rickys bash aliases!

---
+++ .ricky.bash
@@ -0,0 +1,91 @@
+function rv {
+  ruby -v
+}
+
+function tw {
+  twig
+}
+
+function lb {
+  git checkout -
+}
+
+function gm {
+  git checkout master
+}
+
+function nb {
+  git checkout -b $1
+}
+
+function dif {
+  git diff $1 | gitx
+}
+
+function difh {
+  git diff HEAD^ | gitx
+}
+
+function difm {
+  git diff master | gitx
+}
+
+function greset {
+  git reset --soft HEAD^
+}
+
+function gs {
+  git status
+}
+
+function gp {
+  git pull
+}
+
+function testint {
+  umas
+  git branch -D test_int
+  git checkout -b test_int
+  git merge master --no-edit
+  git checkout -
+  git branch -D test_int
+}
+
+function workspace {
+  cd ~/workspace
+}
+
+function rmas {
+  umas
+  git rebase master
+}
+
+function umas {
+  git checkout master
+  git pull
+  git checkout -
+}
+
+function 5times {
+  for i in {1..5}
+    do $*
+  done
+}
+
+function ss {
+  if [ -z "$*" ]
+  then
+    git stash show -p stash@{0} | gitx
+  else
+    git stash show -p stash@{$1} | gitx
+  fi
+}
+
+function sd {
+  if [ -z "$*" ]
+  then
+    git stash drop stash@{0}
+  else
+    git stash drop stash@{$1}
+  fi
+}
