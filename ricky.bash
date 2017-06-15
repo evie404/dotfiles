36a187dc9791d246a6875d7900d22f5c00ac9cdd
Ricky Pai <ricky.pai@airbnb.com>
2017-06-15 13:51:06 -0700

    cleanup

--- ricky.bash
+++ ricky.bash
@@ -36,9 +36,6 @@ export PATH=${GOPATH//://bin:}/bin:$PATH
 # FUCK JAVASCRIPT
 export PATH=~/node_modules/.bin:$PATH
 
-# source /usr/local/share/git-core/git-prompt.sh
-# PS1="[\[\033[32m\]\w]\[\033[0m\]\$(__git_ps1)\n\[\033[1;36m\]\u\[\033[32m\]$ \[\\033[0m\]"
-
 alias bundl="bundle"
 alias budnle="bundle"
 
