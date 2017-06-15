875ec3d04af10973d248de2fc45b6093718e85d5
Ricky Pai <ricky.pai@airbnb.com>
2017-06-15 13:50:38 -0700

    use code for editor

--- ricky.bash
+++ ricky.bash
@@ -21,8 +21,8 @@ fi
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '
 
 # Ruby
-export GEM_EDITOR="subl"
-export BUNDLER_EDITOR="subl"
+export GEM_EDITOR="code"
+export BUNDLER_EDITOR="code"
 export RUBY_CONFIGURE_OPTS="--disable-install-doc --enable-shared"
 
 # GO
