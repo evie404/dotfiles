fa88c8013fb9b695e7fa9fdcd8842383b7bf6848
Ricky Pai <rickyp999+github@gmail.com>
2018-01-02 09:41:27 -0800

    comment out author info

--- ricky.bash
+++ ricky.bash
@@ -58,8 +58,8 @@ export ETCDCTL_API=3
 alias bundl="bundle"
 alias budnle="bundle"
 
-git config --global user.name "Ricky Pai"
-git config --global user.email "rickyp999+github@gmail.com"
+# git config --global user.name "Ricky Pai"
+# git config --global user.email "rickyp999+github@gmail.com"
 
 alias it="git"
 alias gir="git"
