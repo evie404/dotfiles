8b6c9f5ca73905572ad523cb43980e3d0322a1ee
Ricky Pai <rickyp999+github@gmail.com>
2018-01-02 10:06:52 -0800

    include google cloud autocomplete

--- ricky.bash
+++ ricky.bash
@@ -8,6 +8,14 @@ if $(which brew >/dev/null); then
   if [ -f $(brew --prefix)/etc/bash_completion ]; then
     . $(brew --prefix)/etc/bash_completion
   fi
+
+  if [ -f $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc ]; then
+    . $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc
+  fi
+
+  if [ -f $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc ]; then
+    . $(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc
+  fi
 fi
 
 export PROJECTS_DIR="${HOME}/workspace"
