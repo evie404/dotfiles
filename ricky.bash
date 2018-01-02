a98e165484f35f3a9d47d5aa2a21eec5af5c7134
Ricky Pai <rickyp999+github@gmail.com>
2018-01-02 09:50:05 -0800

    rbenv

--- ricky.bash
+++ ricky.bash
@@ -23,6 +23,10 @@ export WORKSPACE=$PROJECTS_DIR
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '
 
 # Ruby
+if $(which rbenv >/dev/null); then
+  eval "$(rbenv init -)"
+fi
+
 export GEM_EDITOR="code"
 export BUNDLER_EDITOR="code"
 export RUBY_CONFIGURE_OPTS="--disable-install-doc --enable-shared"
