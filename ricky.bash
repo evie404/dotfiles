a1c5f4b17116cbe2c60c40eaccb944f579771c3c
Ricky Pai <ricky+github@vsco.co>
2018-08-21 11:26:05 -0700

    more comments

--- ricky.bash
+++ ricky.bash
@@ -25,34 +25,46 @@ ssh-add -K ~/.ssh/id_rsa
 [[ -s ${CURRENT_DIR}/git/aliases.bash ]] && source ${CURRENT_DIR}/git/aliases.bash
 [[ -s ${CURRENT_DIR}/git/helpers.bash ]] && source ${CURRENT_DIR}/git/helpers.bash
 
+# edit the content in this directory
 function edit-bash {
   code ${CURRENT_DIR}
 }
 
+# runs a simple HTTP file server against current directory
 function serveshit {
   python -m SimpleHTTPServer
 }
 
+# runs a command 5 times.
+# example: `5times echo hi`
 function 5times {
   for i in {1..5}
     do $*
   done
 }
 
+# runs a command 10 times.
+# example: `10 times echo hi`
 function 10times {
   for i in {1..10}
     do $*
   done
 }
 
+# curls an endpoint, gives back '<http code>:<total time>' format. useful for quickly checking
+# status code and latency.
 function curl_time {
   curl -o /dev/null -s -w %{http_code}:%{time_total}\\n $*
 }
 
+# converts yaml to json.
+# usage: `yaml2json <input file> <output file>`
 function yaml2json {
   ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' < $1 > $2
 }
 
+# converts json to yaml.
+# usage: `json2yaml <input file> <output file>`
 function json2yaml {
   ruby -ryaml -rjson -e 'puts JSON.parse(ARGF.read).to_yaml' < $1 > $2
 }
