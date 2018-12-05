35ce36236bc80702dc8cb7877c15b9686ad38889
Ricky Pai <ricky+github@vsco.co>
2018-12-05 13:42:30 -0800

    ricky cli

--- modules/golang.bash
+++ modules/golang.bash
@@ -4,6 +4,8 @@ export PATH="$HOME/go/bin/:$PATH" # custom installation location
 export GOPATH="${PROJECTS_DIR}"
 export PATH=${GOPATH//://bin:}/bin:$PATH
 
+export PATH=${GOPATH}/src/github.com/rickypai/ricky-cli/bazel-bin/darwin_amd64_stripped:$PATH
+
 if $(which goenv >/dev/null); then
   eval "$(goenv init -)"
 fi
