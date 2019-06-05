c300d97365e24d113e4cd02470c18886b7442d3d
Ricky Pai <ricky+github@vsco.co>
2019-06-04 17:29:47 -0700

    gv exports

--- modules/graphviz.bash
+++ modules/graphviz.bash
@@ -5,3 +5,11 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 function ginfo {
   cat $1 | gvpr -f ${CURRENT_DIR}/graphinfo.gvpr
 }
+
+function gvpng {
+  find *.gv | xargs -I % sh -c 'dot -Tpng % -Gdpi=300 -o $(echo % | sed "s/gv/png/g")'
+}
+
+function gvsvg {
+  find *.gv | xargs -I % sh -c 'dot -Tsvg % -o $(echo % | sed "s/gv/svg/g")'
+}
