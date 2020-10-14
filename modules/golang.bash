e68903b9255e48c0c6bffada9df186d964075530
Ricky Pai <rickyp999+github@gmail.com>
2020-10-13 23:12:59 -0700

    also add linux paths

--- modules/golang.bash
+++ modules/golang.bash
@@ -5,6 +5,7 @@ export GOPATH="${PROJECTS_DIR}"
 export PATH=${GOPATH//://bin:}/bin:$PATH
 
 export PATH=${GOPATH}/src/github.com/rickypai/ricky-cli/bazel-bin/darwin_amd64_stripped:$PATH
+export PATH=${GOPATH}/src/github.com/rickypai/ricky-cli/bazel-bin/linux_amd64_stripped:$PATH
 
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
 
