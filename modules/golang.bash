1d48b0cd7e2f626c2ff533b072d7798c68153803
Ricky Pai <rickyp999+github@gmail.com>
2020-10-20 15:24:45 -0700

    ricky-cli

--- modules/golang.bash
+++ modules/golang.bash
@@ -4,11 +4,12 @@ export PATH="$HOME/go/bin/:$PATH" # custom installation location
 export GOPATH="${PROJECTS_DIR}"
 export PATH=${GOPATH//://bin:}/bin:$PATH
 
-export PATH=${GOPATH}/src/github.com/rickypai/ricky-cli/bazel-bin/darwin_amd64_stripped:$PATH
-export PATH=${GOPATH}/src/github.com/rickypai/ricky-cli/bazel-bin/linux_amd64_stripped:$PATH
-
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
 
 if $(which goenv >/dev/null); then
   eval "$(goenv init -)"
 fi
+
+function ricky-cli {
+  (cd ~/workspace/src/github.com/rickypai/ricky-cli && go run main.go)
+}
