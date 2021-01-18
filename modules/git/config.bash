bd89679b8be2e0d01613e4eb8dcdd791f363cdc3
Ricky Pai <rickyp999+github@gmail.com>
2021-01-18 07:00:47 -0800

    git prompt

--- modules/git/config.bash
+++ modules/git/config.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# git prompt
+# import git prompt script
+[[ -s /usr/lib/git-core/git-sh-prompt ]] && source /usr/lib/git-core/git-sh-prompt
+
+# tell bash to show user, directory, and git branch
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;33m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[00m\] '
 
 # global git configs. mostly to globally ignore filesystem files
