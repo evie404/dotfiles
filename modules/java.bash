cc7a5c26be0f0a116129053355136c2204b33848
evie404 <wowitsevelynwow+github@gmail.com>
2022-09-02 12:16:23 -0700

    jenv autocomplete

--- modules/java.bash
+++ modules/java.bash
@@ -4,3 +4,5 @@ if $(which jenv >/dev/null); then
   export PATH="$HOME/.jenv/bin:$PATH"
   eval "$(jenv init -)"
 fi
+
+[[ -s /opt/homebrew/opt/jenv/libexec/completions/jenv.bash ]] && source /opt/homebrew/opt/jenv/libexec/completions/jenv.bash 
