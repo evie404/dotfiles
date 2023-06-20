61c845ea8ccc8f94c25832a5d5b0c92e4999abdb
Evelyn Pai <wowitsevelynwow+github@gmail.com>
2023-06-19 21:16:13 -0700

    dont init if pyenv-win

--- modules/python.bash
+++ modules/python.bash
@@ -4,7 +4,9 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
 if $(which pyenv >/dev/null); then
-  eval "$(pyenv init --path)"
+  if [[ $(which pyenv) != *"pyenv-win"* ]]; then
+    eval "$(pyenv init --path)"
 
-  eval "$(pyenv init -)"
+    eval "$(pyenv init -)"
+  fi
 fi
