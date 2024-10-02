d0c1cb30ae3e2c76504c87c3aec390cc1d6b873a
Evelyn Pai <wowitsevelynwow+github@gmail.com>
2024-10-01 17:04:33 -0700

    add pnpm bun stuff

--- modules/javascript.bash
+++ modules/javascript.bash
@@ -14,3 +14,15 @@ export PATH=~/node_modules/.bin:$PATH
 if $(which yarn >/dev/null); then
   export PATH="$PATH:$(yarn global bin)"
 fi
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH=$BUN_INSTALL/bin:$PATH
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+*":$PNPM_HOME:"*) ;;
+*) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
